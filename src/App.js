0254ad5e676a7342fc56b046228d1d7105e54aa5
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-06-15 10:14:23 +0200

    Refactoring, moved state to App from Form

--- src/App.js
+++ src/App.js
@@ -38,18 +38,99 @@ class App extends Component {
     });
   };
 
+  onAddEducation = () => {
+    if (
+      this.state.education.schoolName &&
+      this.state.education.studyTitle &&
+      this.state.education.date
+    ) {
+      this.setState({
+        educations: this.state.educations.concat(this.state.education),
+        education: {
+          id: uniqid(),
+          schoolName: "",
+          studyTitle: "",
+          date: "",
+        },
+      });
+    }
+  };
+
+  // Work experience form functions
+  onAddExperience = () => {
+    this.setState({
+      experiences: this.state.experiences.concat(this.state.experience),
+      experience: {
+        id: uniqid(),
+        company: "",
+        position: "",
+        task: "",
+        start: "",
+        end: "",
+      },
+    });
+  };
+
+  // Form input change handling
+  handleInputChange = (e) => {
+    const group = e.target.getAttribute("data-group");
+    const name = e.target.getAttribute("data-name");
+    this.setState({
+      [group]: {
+        ...this.state[group],
+        [name]: e.target.value,
+      },
+    });
+  };
+
+  // Display input change handling
+  handleDisplayedListChange = (e) => {
+    const id = e.target.getAttribute("data-id");
+    const group = e.target.getAttribute("data-group");
+    const name = e.target.getAttribute("data-name");
+    const newState = this.state[group].map((obj) => {
+      if (obj.id === id) {
+        return { ...obj, [name]: e.target.value };
+      }
+      return obj;
+    });
+    this.setState({
+      [group]: newState,
+    });
+  };
+
   // Render page
   render() {
     if (this.state.main)
       return (
         <div>
-          <Form />
+          <Form
+            general={this.state.general}
+            education={this.state.education}
+            educations={this.state.educations}
+            experience={this.state.experience}
+            experiences={this.state.experiences}
+            onAddEducation={this.onAddEducation}
+            onAddExperience={this.onAddExperience}
+            handleInputChange={this.handleInputChange}
+            handleDisplayedListChange={this.handleDisplayedListChange}
+          />
           <button onClick={this.onSubmit}>{this.state.button}</button>
         </div>
       );
     return (
       <div>
-        {/* <Form /> */}
+        {/* <Form
+          general={this.state.general}
+          education={this.state.education}
+          educations={this.state.educations}
+          experience={this.state.experience}
+          experiences={this.state.experiences}
+          onAddEducation={this.onAddEducation}
+          onAddExperience={this.onAddExperience}
+          handleInputChange={this.handleInputChange}
+          handleDisplayedListChange={this.handleDisplayedListChange}
+        /> */}
         <button onClick={this.onSubmit}>{this.state.button}</button>
       </div>
     );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,3 @@
-import uniqid from "uniqid";
 import { Component } from "react";
 import General from "./General";
 import Education from "./Education";
@@ -7,116 +6,33 @@ import Experience from "./Experience";
 import ExpDisplay from "./ExpDisplay";
 
 export default class Form extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      general: { firstName: "", lastName: "", email: "" },
-      education: { id: uniqid(), schoolName: "", studyTitle: "", date: "" },
-      educations: [],
-      experience: {
-        id: uniqid(),
-        company: "",
-        position: "",
-        task: "",
-        start: "",
-        end: "",
-      },
-      experiences: [],
-    };
-  }
-
-  // Education form functions
-
-  onAddEducation = () => {
-    if (
-      this.state.education.schoolName &&
-      this.state.education.studyTitle &&
-      this.state.education.date
-    ) {
-      this.setState({
-        educations: this.state.educations.concat(this.state.education),
-        education: {
-          id: uniqid(),
-          schoolName: "",
-          studyTitle: "",
-          date: "",
-        },
-      });
-    }
-  };
-
-  // Work experience form functions
-  onAddExperience = () => {
-    this.setState({
-      experiences: this.state.experiences.concat(this.state.experience),
-      experience: {
-        id: uniqid(),
-        company: "",
-        position: "",
-        task: "",
-        start: "",
-        end: "",
-      },
-    });
-  };
-
-  // Form input change handling
-  handleInputChange = (e) => {
-    const group = e.target.getAttribute("data-group");
-    const name = e.target.getAttribute("data-name");
-    this.setState({
-      [group]: {
-        ...this.state[group],
-        [name]: e.target.value,
-      },
-    });
-  };
-
-  // Display input change handling
-  handleDisplayedListChange = (e) => {
-    const id = e.target.getAttribute("data-id");
-    const group = e.target.getAttribute("data-group");
-    const name = e.target.getAttribute("data-name");
-    const newState = this.state[group].map((obj) => {
-      if (obj.id === id) {
-        return { ...obj, [name]: e.target.value };
-      }
-      return obj;
-    });
-    this.setState({
-      [group]: newState,
-    });
-  };
-
   // Render page
   render() {
-    console.log(this.state.experiences);
     return (
       <div className="App">
         <p>All field must be filled for submitting CV.</p>
         <form>
           <General
-            general={this.state.general}
-            handleChange={this.handleInputChange}
+            general={this.props.general}
+            handleChange={this.props.handleInputChange}
           />
           <Education
-            addForm={this.onAddEducation}
-            education={this.state.education}
-            handleChange={this.handleInputChange}
+            addForm={this.props.onAddEducation}
+            education={this.props.education}
+            handleChange={this.props.handleInputChange}
           />
           <EduDisplay
-            educations={this.state.educations}
-            handleChange={this.handleDisplayedListChange}
+            educations={this.props.educations}
+            handleChange={this.props.handleDisplayedListChange}
           />
           <Experience
-            addForm={this.onAddExperience}
-            experience={this.state.experience}
-            handleChange={this.handleInputChange}
+            addForm={this.props.onAddExperience}
+            experience={this.props.experience}
+            handleChange={this.props.handleInputChange}
           />
           <ExpDisplay
-            experiences={this.state.experiences}
-            handleChange={this.handleDisplayedListChange}
+            experiences={this.props.experiences}
+            handleChange={this.props.handleDisplayedListChange}
           />
         </form>
       </div>
