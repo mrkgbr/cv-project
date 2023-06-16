a9358a124d6e3c38ad6796e21c7ddd9a1ffdb274
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-06-16 09:43:10 +0200

    Add style

--- src/components/View/View.js
+++ src/components/View/View.js
@@ -2,14 +2,17 @@ import { Component } from "react";
 import General from "./General-view";
 import Educations from "./Educations";
 import Experiences from "./Experiences";
+import "./View.css";
 
 export default class View extends Component {
   render() {
     return (
-      <div className="View">
-        <General general={this.props.general} />
-        <Educations educations={this.props.educations} />
-        <Experiences experiences={this.props.experiences} />
+      <div>
+        <div className="View">
+          <General general={this.props.general} />
+          <Educations educations={this.props.educations} />
+          <Experiences experiences={this.props.experiences} />
+        </div>
         <button type="button" onClick={this.props.handleClick}>
           {this.props.buttonText}
         </button>
